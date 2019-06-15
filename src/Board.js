e1b052d069e2bb5de5dfcc132c501a52ed3d6ecb
Kenji Kamimura <kjp.kamimura@gmail.com>
2019-06-15 12:29:44 +1200

    rename variables i,j to row,column

--- src/Board.js
+++ src/Board.js
@@ -4,15 +4,15 @@ import Square from "./Square";
 const createBoard = props => {
   const matrixSize = props.squares.length;
   let board = [];
-  for (let i = 0; i < matrixSize; i++) {
+  for (let row = 0; row < matrixSize; row++) {
     let boardRow = [];
     boardRow.push(<div className="board-row" />);
-    for (let j = 0; j < matrixSize; j++) {
+    for (let column = 0; column < matrixSize; column++) {
       boardRow.push(
         <Square
-          key={i + " " + j}
-          isBlack={props.squares[i][j]}
-          onClick={() => props.onClick(i, j)}
+          key={row + " " + column}
+          isBlack={props.squares[row][column]}
+          onClick={() => props.onClick(row, column)}
         />
       );
     }
