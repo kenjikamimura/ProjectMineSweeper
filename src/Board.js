ba3fc611f5204b0956ac292011c4133473b5001b
Kenji Kamimura <kjp.kamimura@gmail.com>
2019-06-15 12:27:28 +1200

    refactor Board.js to be a functional component

--- src/Board.js
+++ src/Board.js
@@ -1,38 +1,26 @@
-import React, { Component } from "react";
+import React from "react";
 import Square from "./Square";
-class Board extends React.Component {
-  renderSquare(i, j) {
-    return (
-      <Square
-        isBlack={this.props.squares[i][j]}
-        onClick={() => this.props.onClick(i, j)}
-      />
-    );
-  }
 
-  createBoard = () => {
-    const matrixSize = this.props.squares.length;
-    let board = [];
-    for (let i = 0; i < matrixSize; i++) {
-      let boardRow = [];
-      boardRow.push(<div className="board-row" />);
-      for (let j = 0; j < matrixSize; j++) {
-        boardRow.push(
-          <Square
-            key={i + " " + j}
-            isBlack={this.props.squares[i][j]}
-            onClick={() => this.props.onClick(i, j)}
-          />
-        );
-      }
-      board.push(boardRow);
+const createBoard = props => {
+  const matrixSize = props.squares.length;
+  let board = [];
+  for (let i = 0; i < matrixSize; i++) {
+    let boardRow = [];
+    boardRow.push(<div className="board-row" />);
+    for (let j = 0; j < matrixSize; j++) {
+      boardRow.push(
+        <Square
+          key={i + " " + j}
+          isBlack={props.squares[i][j]}
+          onClick={() => props.onClick(i, j)}
+        />
+      );
     }
-    return board;
-  };
-
-  render() {
-    return <div>{this.createBoard()}</div>;
+    board.push(boardRow);
   }
-}
+  return board;
+};
 
-export default Board;
+export default function Board(props) {
+  return <div>{createBoard(props)}</div>;
+}
